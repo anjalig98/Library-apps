0f3316e00989a23ce5b327cfc4c1b2063869be2e
anjalig <anjalig1109@gmail.com>
2021-03-30 02:54:49 +0530

    pre-heroku

--- public/js/login.js
+++ public/js/login.js
@@ -16,32 +16,33 @@ function validate(){
     if(regval.test(email.value)){
         erroremail.innerHTML="Email is valid";
         erroremail.style.color="green";
+    }
 
-        if(email.value==""){
+    else if(email.value==""){
             erroremail.innerHTML="Email Cannot be empty!!";
             erroremail.style.color="red";
             return false;
-        }
-
-        else if(strongRegex.test(pass.value)){
-            errorpass.innerHTML="strong";
-            errorpass.style.color="green";
-        }
-        else if(mediumRegex.test(pass.value)){
-            errorpass.innerHTML="Medium";
-            errorpass.style.color="yellow";
-        }  
-        else if(pass.value==""){
-                errorpass.innerHTML="Password cannot be empty";
-                errorpass.style.color="red";
-                return false;
-            }
-        else{
-            errorpass.innerHTML="Password format is not correct";
-            errorpass.style.color="red";
-            return false;
-        }
     }
+
+        // else if(strongRegex.test(pass.value)){
+        //     errorpass.innerHTML="strong";
+        //     errorpass.style.color="green";
+        // }
+        // else if(mediumRegex.test(pass.value)){
+        //     errorpass.innerHTML="Medium";
+        //     errorpass.style.color="yellow";
+        // }  
+        // else if(pass.value==""){
+        //         errorpass.innerHTML="Password cannot be empty";
+        //         errorpass.style.color="red";
+        //         return false;
+        //     }
+        // else{
+        //         errorpass.innerHTML="Password format is not correct";
+        //         errorpass.style.color="red";
+        //         return false;
+        // }
+    
      
     
     else{
@@ -53,21 +54,25 @@ function validate(){
 
     
     
-// var pass = document.getElementById('pass');
-// var errorpass = document.getElementById('errorpass');
-// errorpass.innerHTML="";
-// function pswd(){
-//     var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-//     var mediumRegex = new RegExp("^(?=.{6,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
-//     if(strongRegex.test(pass.value)){
-//         errorpass.innerHTML="";
-//         return true;
-//     }else if(pass.value==""){
-//         errorpass.innerHTML="Password cannot be empty";
-//         return false;
-//     }
-//     else{
-//         errorpass.innerHTML="Password format is not correct";
-//         return false;
-//     }
-// }
+var pass = document.getElementById('pass');
+var errorpass = document.getElementById('errorpass');
+errorpass.innerHTML="";
+function pswd(){
+    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    var mediumRegex = new RegExp("^(?=.{6,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+    if(strongRegex.test(pass.value)){
+        errorpass.innerHTML="strong";
+        errorpass.style.color="green";
+        return true;
+    }
+    else if(pass.value==""){
+        errorpass.innerHTML="Password cannot be empty";
+        errorpass.style.color="red";
+        return false;
+    }
+    else{
+        errorpass.innerHTML="Password format is not correct";
+        errorpass.style.color="red";
+        return false;
+    }
+}
